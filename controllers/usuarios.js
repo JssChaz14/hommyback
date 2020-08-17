0ec280c36709431ed3b2e4ab8a576d047d0e9b11
Edgar Chávez <ecd.teckay@gmail.com>
2020-08-17 15:13:23 -0500

    actualizar y eliminación en usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -52,4 +52,90 @@ const crearUsuarios = async(req, res) => {
 
 }
 
-module.exports = { getUsuarios, crearUsuarios };
+const actualizarUsuario = async(req, res = response) => {
+
+    // TODO: Validar token y comprobar si es el usuario correcto
+
+    const uid = req.params.id;
+
+    try {
+
+        const usuarioDB = await Usuario.findById(uid);
+
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe un usuario por ese id'
+            });
+        }
+
+        // Actualizaciones
+        const { password, google, correo, ...campos } = req.body;
+
+        if (usuarioDB.correo !== correo) {
+
+            const existeEmail = await Usuario.findOne({ correo });
+            if (existeEmail) {
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'Ya existe un usuario con ese email'
+                });
+            }
+        }
+
+        campos.correo = correo;
+        const usuarioActualizado = await Usuario.findByIdAndUpdate(uid, campos, { new: true });
+
+        res.json({
+            ok: true,
+            usuario: usuarioActualizado
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado'
+        })
+    }
+
+}
+
+const borrarUsuario = async(req, res = response) => {
+
+    const uid = req.params.id;
+
+    try {
+
+        const usuarioDB = await Usuario.findById(uid);
+
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe un usuario por ese id'
+            });
+        }
+
+        await Usuario.findByIdAndDelete(uid);
+
+
+        res.json({
+            ok: true,
+            msg: 'Usuario eliminado'
+        });
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+
+    }
+
+
+}
+
+module.exports = { getUsuarios, crearUsuarios, actualizarUsuario, borrarUsuario };
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,7 +4,7 @@ const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 
-const { getUsuarios, crearUsuarios } = require('../controllers/usuarios');
+const { getUsuarios, crearUsuarios, actualizarUsuario, borrarUsuario } = require('../controllers/usuarios');
 
 const router = Router();
 
@@ -14,7 +14,23 @@ router.post('/',
     check('nombre').not().isEmpty(),
     check('correo', 'correo obligatorio').isEmail(),
     validarCampos,
-    crearUsuarios)
+    crearUsuarios);
+
+router.put('/:id',
+    // validarJWT,
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('correo', 'El email es obligatorio').isEmail(),
+    // check('role', 'El role es obligatorio').not().isEmpty(),
+    validarCampos,
+
+    actualizarUsuario
+);
+
+router.delete('/:id',
+    // validarJWT,
+    borrarUsuario
+);
+
 
 
 module.exports = router;
